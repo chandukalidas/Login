7d4224e6bc69660bd2b642ad75cdc17e06b4cb3c
chandukalidas <100261832+chandukalidas@users.noreply.github.com>
2023-02-04 10:42:32 +0530

    comment added....

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+//this program will validate the input and allow the user to access if it matches.
 function validate(){
     var username=document.getElementById("username").value;
     var pass=document.getElementById("password").value;
